8134456c953c39fee929401c8c84cba575cdf011
Jonathan Dahan <hi@jonathan.is>
2018-12-05 14:34:41 -0500

    day-02

---
+++ 02-inventory-management-system/checksum.js
@@ -0,0 +1,33 @@
+const fs = require('fs')
+const { promisify } = require('util')
+const readFile = promisify(fs.readFile)
+
+const read = async filename => {
+  const bytes = await readFile(filename)
+  const input = bytes.toString().split('\n')
+  input.pop() // deal with extra newline
+  return { input }
+}
+
+const has = what => counts => counts.includes(what) ? 1 : 0
+const checksums = [has(2), has(3)]
+
+// Take a string, and give back a 26 character array of all the counts of the letters
+const count = string => string.split('')
+  .reduce((counts, letter) => {
+    counts[letter.charCodeAt(0) - 97]++
+    return counts
+  }, new Array(26).fill(0))
+
+const reduce = ({input}) => {
+  const sums = input
+    .map(count)
+    .map(count => checksums.map(checksum => checksum(count)))
+    .reduce((sums, sum) => sums.map((_, i) => sums[i] + sum[i]), [0, 0])
+
+  return sums[0] * sums[1]
+}
+
+read('input')
+  .then(reduce)
+  .then(console.log)
